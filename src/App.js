03aeab9b2429b415405c2c4521405c4844882c05
Anna Anderwald <anna.v.anderwald@gmail.com>
2021-07-06 11:38:27 -0400

    made a change to try and check it out

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Not sure how I feel yet. I do like things are already loaded.</p>
+        <p>Not sure how I feel yet. </p>
         <a
           className="App-link"
           href="https://reactjs.org"
